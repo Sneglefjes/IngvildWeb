a3242006980dcf1ec6c8c2d9aa550887320308d8
Sneglefjes <63294978+Sneglefjes@users.noreply.github.com>
2020-11-26 17:20:59 +0100

    Add files via upload

---
+++ kode.js
@@ -0,0 +1,27 @@
+// Her finner jeg knappen og overskriften, med id-verdien
+// document.querySelector velger bare den første som treffer
+// så om det er flere med samme id (er egentlig ikke lov, men kjører helt fint)
+// så får man bare den første
+const knappen = document.querySelector("#knappen")
+const skriften = document.querySelector("#skriften")
+
+// lager et array med strenger (data for flere bokstaver etterhverandre), som jeg kan hente fra når man trykker knappen
+const skrifter = ["Underskrift", "Overskrift", "Høyreskrift", "Venstreskrift", "Bakskrift", "Forranskrift", "Inniskrift", "Utenforskrift"]
+
+// Her sier vi at hver gang noen trykker på knappen, så skal vi kjøre byttSkrift-funksjonen
+knappen.addEventListener("click", byttSkrift)
+
+
+function byttSkrift() {
+    // Her henter jeg ut det som står i overskriften nå, så jeg ikke bytter til det samme som var før
+    const detSomStårNå = skriften.textContent
+
+    // Her finner jeg bare de tekstene som ikke er verdien til detSomStårNå i skrifter-lista
+    muligSkrift = skrifter.filter(skrift => skrift !== detSomStårNå)
+
+    // Her velger jeg en tilfeldig verdi fra lista med nye tekster
+    nesteSkrift = muligSkrift[Math.floor((Math.random()*muligSkrift.length))]
+
+    // Her oppdaterer jeg overskriften med den verdien vi plukket
+    skriften.textContent = nesteSkrift;
+}
